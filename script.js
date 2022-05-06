b8be20918e713ab1154c986fbd5efdd561dfb9cd
Fedor <fed-sav@yandex.ru>
2022-05-06 18:45:02 +0300

    calendar sample

--- script.js
+++ script.js
@@ -1,20 +1,12 @@
 'use strict'
 let date = new Date();
-let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-let weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Wednesday', 'Friday', 'Saturday'];
 
 
-let monthDate = date.getMonth();
-let weekDayDate = date.getDay();
 let day = date.getDate();
 let year = date.getFullYear();
 
 
-let month = months[monthDate];
-let weekDay = weekDays[weekDayDate];
-
-
-document.getElementById('month').textContent = month;
-document.getElementById('weekDay').textContent = weekDay;
+document.getElementById('month').textContent = date.toLocaleDateString('en-US', {month: "long"})
+document.getElementById('weekDay').textContent = date.toLocaleDateString('en-US', {weekday: 'long'});
 document.getElementById('day').textContent = day;
 document.getElementById('year').textContent = year;
